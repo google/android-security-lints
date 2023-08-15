33527d31cd9e255d3c76c8930aeda25de7dcff5c
Bessie Jiang <bessiej@google.com>
2023-08-15 17:49:12 +0000

    Update example Android code that will trigger some of the new custom lint checks

---
+++ app/src/main/java/com/android/example/FakeFileProvider.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.example
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class FakeFileProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun query(p0: Uri, p1: Array<out String>?, p2: String?, p3: Array<out String>?, p4: String?): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+}
